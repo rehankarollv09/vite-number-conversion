e2884627c9a5c1bcb2d134f68e3fc1dc88573119
rehankarollv09 <rehan.karol@launchventures.co>
2023-05-01 00:28:10 +0530

    Refractor code

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,8 +1,46 @@
+import { useState } from "react"
+
 const Login= ()=>{
+    const [inputValue,setInputValue] = useState({
+        email:"",
+        password:""
+    })
+    const [errors,setErrors]=useState("")
+  function onSubmit(e:React.FormEvent<HTMLFormElement>):void{
+    e.preventDefault();
+    setErrors('')
+    if(inputValue.email==='' ||  inputValue.password===''){
+       setErrors('Please enter compulsory fields')
+       return;
+    }
+
+    if(inputValue.email==='rehan' && inputValue.password==='rehan') 
+    {
+        
+     return;
+    }
+    setErrors('Invalid Username password')
+     
+  }
+
+  function onInputChange(e:React.ChangeEvent<HTMLInputElement>):void{
+    e.preventDefault()
+    console.log(e.target.name)
+      setInputValue({
+        ...inputValue,
+        [e.target.name]:e.target.value
+      })
+ }
     return(
         <>
         <h1>
             Login Form
+            <form onSubmit={onSubmit}>
+                <input type={'text'} placeholder="email" value={inputValue.email} name='email' onChange={onInputChange}/>
+                <input type={"text"} placeholder='password' value={inputValue.password} name='password' onChange={onInputChange}/>
+                <input type="submit" value='Login'/>
+            </form>
+            {errors&&<p>{errors}</p>}
         </h1>
         </>
     )
--- src/test/__tests__/login.spec.tsx
+++ src/test/__tests__/login.spec.tsx
@@ -1,11 +1,46 @@
-import '@testing-library/jest-dom';
-import Login from '../../Login';
-import {render} from '@testing-library/react'
-describe("Login Form",()=>{
-    it("should render Login Form without crashing",()=>{
-        expect(()=>{
-            render(<Login/>)
-        }).not.toThrow()
-    })
-})
+import { render, fireEvent } from "@testing-library/react";
+import Login from '../../Login'
 
+describe("Login component", () => {
+  it("displays error message when required fields are not filled in", () => {
+    const { getByText,getByPlaceholderText } = render(<Login />);
+
+    const emailInput = getByPlaceholderText("email");
+    const passwordInput = getByPlaceholderText("password");
+    const submitButton = getByText("Login");
+
+    fireEvent.change(emailInput, { target: { value: "" } });
+    fireEvent.change(passwordInput, { target: { value: "" } });
+    fireEvent.click(submitButton);
+
+    expect(getByText("Please enter compulsory fields")).toBeInTheDocument();
+  });
+
+  it("displays error message when incorrect credentials are entered", () => {
+    const { getByText, getByLabelText } = render(<Login />);
+
+    const emailInput = getByLabelText("email");
+    const passwordInput = getByLabelText("password");
+    const submitButton = getByText("Login");
+
+    fireEvent.change(emailInput, { target: { value: "wrong" } });
+    fireEvent.change(passwordInput, { target: { value: "wrong" } });
+    fireEvent.click(submitButton);
+
+    expect(getByText("Invalid Username password")).toBeInTheDocument();
+  });
+
+  it("logs in when correct credentials are entered", () => {
+    const { getByText, getByLabelText } = render(<Login />);
+
+    const emailInput = getByLabelText("email");
+    const passwordInput = getByLabelText("password");
+    const submitButton = getByText("Login");
+
+    fireEvent.change(emailInput, { target: { value: "rehan" } });
+    fireEvent.change(passwordInput, { target: { value: "rehan" } });
+    fireEvent.click(submitButton);
+
+    expect(console.log).toHaveBeenCalledWith("Login");
+  });
+});
